2e4fea30f7d9cab6738586c32fe88f2a0d90090a
Gareth Lysnkey <lynskey123@gmail.com>
2016-11-03 11:13:56 +0000

    Application Update
    Added Common and Database class. Have to finish Database class.

--- DictionaryApp/DictionaryApp/Helper/Common.cs
+++ DictionaryApp/DictionaryApp/Helper/Common.cs
@@ -3,11 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Popups;
 
 namespace DictionaryApp.Helper
 {
     public class Common
     {
+        //Db name
+        public static string DB_NAME = "Database.db";
+        //Column names
+        public static string SpeakRateColumn = "SpeakRate";
+        public static string SpeakPitchColumn = "SpeakPitch";
+        public static string SpeakRangeColumn = "SpeakRange";
+        public static string SpeakVolumeColumn = "SpeakVolume";
+        public static string SpeakRateGender = "VoiceGender";
+        public static string WordFontSize = "WordFontSize";
+        public static string TypeFontSize = "TypeFontSize";
+        public static string DescriptionFontSize = "DescriptionFontSize";
+        public static string LanguageTo = "LanguageTo";
+
+        //pivot index
+        public static int HomeIndex = 0;
+        public static int SearchIndex = 1;
+        public static int FavouritesIndex = 2;
+        public static int RecentsIndex = 3;
+        public static int MainPivotSaveIndex = 0;
+
+        //Function to show message dialog
+        public static async void showMessage(string errorMessage)
+        {
+            var msg = new MessageDialog(errorMessage);
+            var okBtn = new UICommand("OK");
+            msg.Commands.Add(okBtn);
+            try
+            {
+                IUICommand result = await msg.ShowAsync();
+            }
+            catch
+            {
+                return;
+            }
+
+        }
 
     }
 }
---
+++ DictionaryApp/DictionaryApp/Helper/Database.cs
@@ -0,0 +1,84 @@
+﻿using SQLitePCL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryApp.Helper
+{
+    public class Database
+    {
+        public static List<Words> getAllWords(string tableName)
+        {
+            List<Words> result = new List<Words>();
+            try
+            {
+                using (var connection = new SQLiteConnection(Windows.Storage.ApplicationData.Current.LocalFolder.Path + "\\" + Common.DB_NAME))
+                {
+                    var statement = connection.Prepare("SELECT * FROM" +tableName);
+                    while(!(SQLiteResult.DONE == statement.Step()))
+                    {
+                        if(statement[0] != null)
+                        {
+                            Words word = new Words();
+                            word.Id = Convert.ToInt32(statement["Id"]);
+                            word.Word = statement["Word"].ToString();
+                            word.Type = statement["Type"].ToString();
+                            word.Description = statement["Description"].ToString();
+                            result.Add(word);
+                        }
+                    }
+                    return result;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static List<string> getGroupChar(string tableName)
+        {
+            List<string> result = new List<string>();
+            try
+            {
+                using (var connection = new SQLiteConnection(Windows.Storage.ApplicationData.Current.LocalFolder.Path + "\\" + Common.DB_NAME))
+                {
+                    var statement = connection.Prepare("SELECT DISTINCT UPPER(SUBSTR(Word,1,1)) AS GroupChar FROM " + tableName);
+                    while (!(SQLiteResult.DONE == statement.Step()))
+                    {
+                        if (statement[0] != null)
+                        {
+                            result.Add(statement["GroupChar"].ToString());
+                        }
+                    }
+                    return result;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static int deleteFromTable(string TableName, Words word)
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(Windows.Storage.ApplicationData.Current.LocalFolder.Path + "\\" + Common.DB_NAME))
+                {
+                    var query = string.Format("DELETE FROM {0} WHERE ID='{1}'",TableName, word.Id);
+                    var statement = connection.Prepare(query);
+                    statement.Step();
+                    return 1;
+                }
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+
+    }
+}
