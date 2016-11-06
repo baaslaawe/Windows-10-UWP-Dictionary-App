62b7e35300423b5a326fc2c1cd99d68312163d4e
Gareth Lysnkey <lynskey123@gmail.com>
2016-11-06 22:31:36 +0000

    ViewWord.xaml.cs
    event handlers

--- DictionaryApp/DictionaryApp/Views/ViewWord.xaml.cs
+++ DictionaryApp/DictionaryApp/Views/ViewWord.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,160 @@ namespace DictionaryApp.Views
     {
         private Words word;
         private int isFavourite;
+
+        private void loadWordsDetails(Words word)
+        {
+            txtWord.Text = word.Word;
+            txtType.Text = word.Type;
+            txtDescription.Text = word.Description;
+        }
+
+        private string convertIntToValue(string type, int value)
+        {
+            string result = "";
+            if (type.Equals("pitch"))
+            {
+                switch (value)
+                {
+                    case 1:
+                        result = "x-low";
+                        break;
+                    case 2:
+                        result = "low";
+                        break;
+                    case 3:
+                        result = "medium";
+                        break;
+                    case 4:
+                        result = "high";
+                        break;
+                    case 5:
+                        result = "x-high";
+                        break;
+                    default:
+                        result = "default";
+                        break;
+                }
+                
+                        
+            }
+            else if (type.Equals("range"))
+            {
+                switch (value)
+                {
+                    case 1:
+                        result = "x-low";
+                        break;
+                    case 2:
+                        result = "low";
+                        break;
+                    case 3:
+                        result = "medium";
+                        break;
+                    case 4:
+                        result = "high";
+                        break;
+                    case 5:
+                        result = "x-high";
+                        break;
+                    default:
+                        result = "default";
+                        break;
+                }
+            }
+            else if (type.Equals("rate"))
+            {
+                switch (value)
+                {
+                    case 1:
+                        result = "x-slow";
+                        break;
+                    case 2:
+                        result = "slow";
+                        break;
+                    case 3:
+                        result = "medium";
+                        break;
+                    case 4:
+                        result = "fast";
+                        break;
+                    case 5:
+                        result = "x-fast";
+                        break;
+                    default:
+                        result = "default";
+                        break;
+                }
+            }
+            else if (type.Equals("volume"))
+            {
+                switch (value)
+                {
+                    case 0:
+                        result = "silent";
+                        break;
+                    case 1:
+                        result = "x-soft";
+                        break;
+                    case 2:
+                        result = "soft";
+                        break;
+                    case 3:
+                        result = "medium";
+                        break;
+                    case 4:
+                        result = "loud";
+                        break;
+                    case 5:
+                        result = "x-loud";
+                        break;
+                    default:
+                        result = "default";
+                        break;
+                }
+            }
+            else
+            {
+                result = "default";
+            }
+            return result;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            //handle back button
+            SystemNavigationManager.GetForCurrentView().BackRequested += ViewWord_BackRequested;
+            word = e.Parameter as Words;
+            loadWordsDetails(word);
+            isFavourite = Database.searchFavourites(word);
+
+            //check favourites
+            if(isFavourite == 1)
+            {
+                btnFavourites.Icon = new SymbolIcon(Symbol.UnFavorite);
+                btnFavourites.Label = "UnFavourite";
+            }
+            else
+            {
+                btnFavourites.Icon = new SymbolIcon(Symbol.Favorite);
+                btnFavourites.Label = "Favourite";
+            }
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            SystemNavigationManager.GetForCurrentView().BackRequested -= ViewWord_BackRequested;
+        }
+
+        private void ViewWord_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            e.Handled = true;
+            if (Frame.CanGoBack)
+            {
+                Frame.GoBack();
+            }
+        }
+
         public ViewWord()
         {
             this.InitializeComponent();
@@ -37,7 +192,18 @@ namespace DictionaryApp.Views
 
         private void btnFavourites_Click(object sender, RoutedEventArgs e)
         {
-
+            if(isFavourite == 0)
+            {
+                Database.insertIntoTable("Favourites", word);
+                btnFavourites.Icon = new SymbolIcon(Symbol.UnFavorite);
+                btnFavourites.Label = "UnFavourites";
+            }
+            else
+            {
+                Database.deleteFromTable("Favourites", word);
+                btnFavourites.Icon = new SymbolIcon(Symbol.Favorite);
+                btnFavourites.Label = "Favourites";
+            }
         }
 
         private void btnTranslate_Click(object sender, RoutedEventArgs e)
